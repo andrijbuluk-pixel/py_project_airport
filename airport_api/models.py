987242b8060459a0855ae650a06f0568dbf8a930
Andriy <andrijbuluk@gmail.com>
2026-07-20 20:10:48 +0200

    Aded: using the User model

--- airport_api/models.py
+++ airport_api/models.py
@@ -1,8 +1,6 @@
-from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
-class User(AbstractUser):
-    pass
 
 class Crew(models.Model):
     first_name = models.CharField(max_length=100)
@@ -70,7 +68,10 @@ class Flight(models.Model):
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return f"Order - {self.user} | {self.created_at}"
