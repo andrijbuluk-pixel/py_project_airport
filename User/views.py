666395ef965f5720b79a13923a823663fa622cc3
Andriy <andrijbuluk@gmail.com>
2026-07-22 17:48:37 +0200

    Fix: permission_classes for UserViewSet with tuple on list

--- User/views.py
+++ User/views.py
@@ -12,7 +12,7 @@ class CreateUser(generics.CreateAPIView):
 class UserViewSet(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     authentication_classes =(JWTAuthentication,)
-    permission_classes = (IsAuthenticated)
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return self.request.user
