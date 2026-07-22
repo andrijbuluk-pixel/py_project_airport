5a62ddda250eeeb31ac464e2f0daba43b0788a8b
Andriy <andrijbuluk@gmail.com>
2026-07-22 10:17:32 +0200

    Update: uniqueness of fields

--- airport_api/models.py
+++ airport_api/models.py
@@ -3,22 +3,22 @@ from django.db import models
 
 
 class Crew(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
 class AirplaneType(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return f"Type - {self.name}"
 
 
 class Airplane(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
     airplane_type = models.ForeignKey(
@@ -31,8 +31,8 @@ class Airplane(models.Model):
 
 
 class Airport(models.Model):
-    name = models.CharField(max_length=200)
-    closest_big_city = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
+    closest_big_city = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
         return f"Airport - {self.name}"
