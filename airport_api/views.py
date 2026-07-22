7cb78bd7afe4ab63533b93321a0fddf53dccd9d2
Andriy <andrijbuluk@gmail.com>
2026-07-22 09:48:18 +0200

    Aded: for all views permisions besides 2 last classes

--- airport_api/views.py
+++ airport_api/views.py
@@ -22,35 +22,43 @@ from airport_api.models import (
     Ticket,
 )
 
+from airport_api.permissions import IsAdminOrIfAuthenticatedReadOnly
+
 
 class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
 
 
 class AirplaneTypeViewSet(viewsets.ModelViewSet):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
 
 
 class AirplaneViewSet(viewsets.ModelViewSet):
     queryset = Airplane.objects.all()
     serializer_class = AirplaneSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
 
 
 class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
 
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
 
 
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
 
 
 class OrderViewSet(viewsets.ModelViewSet):
