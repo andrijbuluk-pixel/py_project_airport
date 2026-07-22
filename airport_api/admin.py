959a7228cf40de5df4b0a155eaa25af57c60a6fa
Andriy <andrijbuluk@gmail.com>
2026-07-22 17:25:49 +0200

    register model for admin panel

--- airport_api/admin.py
+++ airport_api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport_api.models import (
+    Crew,
+    AirplaneType,
+    Airplane,
+    Airport,
+    Route,
+    Flight,
+    Order,
+    Ticket,
+)
+
+admin.site.register(Crew)
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Airport)
+admin.site.register(Route)
+admin.site.register(Flight)
+admin.site.register(Order)
+admin.site.register(Ticket)
